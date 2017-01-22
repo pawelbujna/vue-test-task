ea29470d8673e1728b6466ee0b2206a6937d541b
Pawel Bujna <pawelbujna@pc4.home>
2017-01-22 18:17:59 +0100

    Vue app + lodash - init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  import _ from 'lodash';
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        users: ['john', 'paul', 'jacub'],
+        admins: ['jacob', 'paul', 'tom']
+      }
+    },
+    created() {
+      this.takeSimilar();
+    },
+    methods: {
+      takeSimilar() {
+        console.log(_.difference(this.users, this.admins));
+      }
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
