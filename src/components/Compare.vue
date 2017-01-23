3345c03f30b1b5c1338aee634df6db3955b1bc8e
Pawel Bujna <pawelbujna@pc4.home>
2017-01-23 18:55:46 +0100

    Displaying same actors in two different movies

--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -1,15 +1,15 @@
 <template>
-    <form>
-        <button @click="getAllMoviesData()">
-                test
+    <form @submit.prevent="onSubmit">
+        <button type="submit">
+                Compare two movies.
         </button>
         <div>
             <label for="title-one">Pick first title</label>
-            <input type="text" name="title-one">
+            <input type="text" name="title-one" v-model="titleOne">
         </div>
         <div>
             <label for="title-two">Pick second title</label>
-            <input type="text" name="title-two">
+            <input type="text" name="title-two" v-model="titleTwo">
         </div>
         <p v-if="noSameActors">
             There aren't same actors in these movies.
@@ -27,13 +27,27 @@
     export default {
         data() {
             return {
+                titleOne: '',
+                titleTwo: '',
+                correctedTitleOne: '',
+                correctedTitleTwo: '',
                 sameActors: [],
                 movieOneActors: [],
                 movieTwoActors: [],
-                noSameActors: false
+                noSameActors: false,
+                apiPath: 'http://www.omdbapi.com/?t='
             }
         },
         methods: {
+            onSubmit() {
+                this.getAllMoviesData()
+            },
+            updateMovieOneTitle() {
+                this.correctedTitleOne = this.titleOne.toLowerCase().split(" ").join("+");
+            },
+            updateMovieTwoTitle() {
+                this.correctedTitleTwo = this.titleTwo.toLowerCase().split(" ").join("+");
+            },
             checkForSameActors() {
                 if (this.sameActors.length !== 0) {
                     this.noSameActors = false;
@@ -41,20 +55,31 @@
                     this.noSameActors = true;
                 }
             },
-            getSameActors(movieOneActors, movieTwoActors) {
-                this.sameActors = [];
-                this.movieOneActors.push(movieOneActors);
-                this.movieTwoActors.push(movieTwoActors);
+            getSameActors(actorsFromFirstMovie, actorsFromSecondMovie) {
+                if (!actorsFromFirstMovie && !actorsFromSecondMovie) {
+                    return;
+                }
+
+                delete this.movieOneActors;
+                delete this.movieTwoActors;
+                delete this.sameActors;
+
+
+                this.movieOneActors = actorsFromFirstMovie.split(', ')
+                this.movieTwoActors = actorsFromSecondMovie.split(', ')
                 this.sameActors = _.intersection(this.movieOneActors, this.movieTwoActors)
+                // console.log(this.sameActors);
                 this.checkForSameActors();
             },
             getMovieOneData() {
-                let movieOneTitle = [];
-                return axios.get(`http://www.omdbapi.com/?t=gladiator`);
+                this.updateMovieOneTitle()
+                // console.log(`${this.apiPath}${this.correctedTitleOne}`);
+                return axios.get(`${this.apiPath}${this.correctedTitleOne}`);
             },
             getMovieTwoData() {
-                let movieTwoTitle = [];
-                return axios.get(`http://www.omdbapi.com/?t=harry+potter`);
+                this.updateMovieTwoTitle()
+                // console.log(`${this.apiPath}${this.correctedTitleTwo}`);
+                return axios.get(`${this.apiPath}${this.correctedTitleTwo}`);
             },
             getAllMoviesData() {
                 axios.all([this.getMovieOneData(), this.getMovieTwoData()])
