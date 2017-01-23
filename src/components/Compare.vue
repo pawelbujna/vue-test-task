2c49f3c8c53f2730d34e3d860d7974e90a2f35ff
Pawel Bujna <pawelbujna@pc4.home>
2017-01-23 12:28:16 +0100

    HTTP requests working. Comparing actors.

--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -1,5 +1,8 @@
 <template>
-    <!--<form>
+    <form>
+        <button @click="getAllMoviesData()">
+                test
+        </button>
         <div>
             <label for="title-one">Pick first title</label>
             <input type="text" name="title-one">
@@ -8,43 +11,58 @@
             <label for="title-two">Pick second title</label>
             <input type="text" name="title-two">
         </div>
-        <div>
-            <p v-for="movie in movieOneData">
-                {movie}
-            </p>
-        </div>
-        <p>
+        <p v-if="noSameActors">
+            There aren't same actors in these movies.
         </p>
-    </form>-->
-    <div>
-        <p v-for="movie in movieOneData">
-            {{movie}}
+        <p v-if="!noSameActors" v-for="actor in sameActors">
+            {{actor}}
         </p>
-        <button @click="submit()">
-                test
-        </button>
-    </div>
+    </form>
 </template>
 
 <script>
     import axios from 'axios';
+    import _ from 'lodash';
 
     export default {
         data() {
             return {
-                movieOneData: [],
-                movieTwoData: []
+                sameActors: [],
+                movieOneActors: [],
+                movieTwoActors: [],
+                noSameActors: false
             }
         },
         methods: {
-            submit() {
-                axios.get(`http://www.omdbapi.com/?t=gladiator`)
-                    .then((response) => {
-                        this.movieOneData = response.data.Actors.split(',')
-                    })
+            checkForSameActors() {
+                if (this.sameActors.length !== 0) {
+                    this.noSameActors = false;
+                } else if (this.sameActors.length === 0) {
+                    this.noSameActors = true;
+                }
+            },
+            getSameActors(movieOneActors, movieTwoActors) {
+                this.sameActors = [];
+                this.movieOneActors.push(movieOneActors);
+                this.movieTwoActors.push(movieTwoActors);
+                this.sameActors = _.intersection(this.movieOneActors, this.movieTwoActors)
+                this.checkForSameActors();
+            },
+            getMovieOneData() {
+                let movieOneTitle = [];
+                return axios.get(`http://www.omdbapi.com/?t=gladiator`);
+            },
+            getMovieTwoData() {
+                let movieTwoTitle = [];
+                return axios.get(`http://www.omdbapi.com/?t=harry+potter`);
+            },
+            getAllMoviesData() {
+                axios.all([this.getMovieOneData(), this.getMovieTwoData()])
+                    .then(axios.spread((movieOne, movieTwo) => {
+                        this.getSameActors(movieOne.data.Actors, movieTwo.data.Actors)
+                    }));
             }
         }
-
     }
 
 </script>
