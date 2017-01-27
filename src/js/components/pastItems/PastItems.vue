a999d1f11d7f10ab94121f656c15f11e01822229
Pawel Bujna <pawelbujna@pc4.home>
2017-01-27 14:21:01 +0100

    Added firebase clearing function

--- src/js/components/pastItems/PastItems.vue
+++ src/js/components/pastItems/PastItems.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="m-t-10 m-b-5 p-t-10 p-b-5 p-l-10 p-r-10 bg-col-primary" v-if="items.length > 0">
+    <div class="m-t-10 m-b-5 p-t-10 p-b-5 p-l-10 p-r-10 bg-col-primary" v-if="items.length > 0 && !isLoading">
+        <button class="button m-b-5" @click="clearData()">clear data</button>
         <h1 class="col-w m-b-10 fs-10">Movies you were looking for: </h1>
         <p class="col-w m-b-5" v-for="item in items">
             {{item}}
         </p>
+    </div>
+    <div v-else-if="isLoading">
+        <p class="col-w m-t-5 m-b-5">Loading...</p>
     </div>
 </template>
 
@@ -11,26 +15,32 @@
     import firebase from 'firebase';
     import _ from 'lodash';
 
-    let firebaseRef = firebase.database().ref('/')
+    let firebaseRef = firebase.database().ref()
 
     export default {
         data() {
             return {
-                items: []
+                items: [],
+                isLoading: true
+            }
+        },
+        methods: {
+            clearData() {
+                firebaseRef.child('movies').remove();
+                this.items = [];
             }
         },
         mounted() {
+
             firebaseRef.on('value', (response) => {
                 let responseData = response.val();
 
                 if (responseData) {
                     this.items =  Object.keys(responseData.movies).map(x => responseData.movies[x].title)
                 }
+
+                this.isLoading = false;
             });
         }
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
